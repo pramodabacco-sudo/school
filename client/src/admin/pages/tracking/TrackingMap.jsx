572fa8ad601f62f665be421b23743a87277425be
pramod <pramod.abacco@gmail.com>
2026-04-29 01:27:00 +0530

    update

--- client/src/admin/pages/tracking/TrackingMap.jsx
+++ client/src/admin/pages/tracking/TrackingMap.jsx
@@ -76,7 +76,12 @@ const TrackingMap = () => {
         center={[12.9716, 77.5946]}
         zoom={12}
         // ✅ FIX 4: inline style guarantees height is applied
-        style={{ width: "100%", height: "100%", minHeight: "400px" }}
+       style={{
+        width: "100%",
+        height: "100%",
+        minHeight: "400px",
+        zIndex: 1,
+      }}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
